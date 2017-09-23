5be1f9215126a4c832e07843e2d6be5b7cefda9e
John Potts <john@arlingtonchurch.com>
2017-09-23 08:11:04 -0400

    initial commit

--- OutputParameters/Program.cs
+++ OutputParameters/Program.cs
@@ -10,6 +10,41 @@ namespace OutputParameters
     {
         static void Main(string[] args)
         {
+            string name, gender;
+            GetData(out name, out gender);
+            int age = GetAge();
+            Console.WriteLine("Name: {0} \n Age: {1} \n Gender: {2}", name, age, gender);
+            Console.ReadLine();
+        }
+       
+
+
+
+        private static int GetAge()
+        {
+            bool valid = false;
+            int number = 0;
+            while (!valid)
+            {
+                Console.WriteLine("Enter your age: ");
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("That is not an integer!");
+                }
+            }
+            return number;
+        }
+
+        private static void GetData(out string name, out string gender)
+        {
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your gender");
+            gender = Console.ReadLine();
         }
     }
 }
